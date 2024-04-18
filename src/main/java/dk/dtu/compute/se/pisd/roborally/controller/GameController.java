56db961d2e947a49afc093ba943fd1c796dee894
Frebbers <frederik.bode@hendrichsen.dk>
2024-04-18 18:33:45 +0200

    Added missing exception from handout

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -309,4 +309,20 @@ public class GameController {
         assert false;
     }
 
+
+
+    class ImpossibleMoveException extends Exception {
+
+        private Player player;
+        private Space space;
+        private Heading heading;
+
+        public ImpossibleMoveException(Player player, Space space, Heading heading) {
+            super("Move impossible");
+            this.player = player;
+            this.space = space;
+            this.heading = heading;
+        }
+    }
+
 }

723a769cc3e4586ecbf130fe9bbac2ed916ed0ec
Freb <s224804@dtu.dk>
2024-04-25 14:49:29 +0200

    Finished ConveyorBeltController

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ConveyorBeltController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ConveyorBeltController.java
@@ -45,6 +45,14 @@ public class ConveyorBeltController extends FieldAction {
         this.heading = heading;
     }
 
+    /**
+     * Perform the action related to the conveyor belt on the given space
+     *
+     * @param gameController the game controller
+     * @param space          the space with the conveyor belt
+     * @return true if the action was successfully performed, false otherwise
+     * @author s224804 Frederik Bode Hendrichsen
+     */
     @Override
     public boolean doAction(@NotNull GameController gameController, @NotNull Space space) {
         // Perform the action related to the conveyor belt on the given space
@@ -56,30 +64,16 @@ public class ConveyorBeltController extends FieldAction {
         // You might also need to access the player on the space
         Player player = space.getPlayer();
 
-        // Perform the action, such as moving the player in the direction of the conveyor belt
+        // Attempt to the player in the direction of the conveyor belt
         if (player != null) {
-            Heading conveyorBeltHeading = this.getHeading(); // Get the heading of the conveyor belt
-            Heading playerHeading = player.getHeading(); // Get the heading of the player
-
-            // Perform logic to determine the new space for the player based on the conveyor belt direction
-            // For example:
-            // Move the player to the next space in the conveyor belt direction
-            Space nextSpace = board.getNeighbour(space, conveyorBeltHeading);
-            if (nextSpace != null) {
-                nextSpace.setPlayer(player);
-                space.setPlayer(null); // Remove the player from the current space
-                // You might need to handle collision or other specific logic here
-                return true; // Return true to indicate that the action was successfully performed
-            } else {
-                // Handle the case when there is no next space (e.g., the conveyor belt is at the edge of the board)
-                // You might need to adjust the logic based on your game's requirements
-                return false; // Return false to indicate that the action could not be performed
+            if (gameController.boardController.handleMovement
+                    (space, board.getNeighbour(space, this.heading), this.heading)) {
+                return true;
             }
-        } else {
-            // Handle the case when there is no player on the conveyor belt space
-            // You might also return true or false based on your game's requirements
-            return false;
-        }
-    }
 
+        }
+        //No player on the conveyor belt
+        return false;
+
+    }
 }

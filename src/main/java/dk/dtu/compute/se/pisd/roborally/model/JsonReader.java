48a7d3df0ab59de96b7303902c542f282894522b
FrederikVA <s224308@student.dtu.dk>
2024-04-25 16:21:30 +0200

    yes

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/JsonReader.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/JsonReader.java
@@ -4,15 +4,19 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import dk.dtu.compute.se.pisd.roborally.controller.AppController;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class JsonReader {
+    /**
+     * This is a class for reading our Json Boards with gson. *
+     * It creates json objects and arrays for each
+     *
+     * @author s213364, s224308
+     */
 
     private final int boardNumber;
 

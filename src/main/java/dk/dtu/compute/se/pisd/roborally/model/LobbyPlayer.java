e864766a61ac6e443971afeb6eea712238ceb824
Frebbers <frederik.bode@hendrichsen.dk>
2024-06-12 21:07:49 +0200

    finished LobbyPlayer.java state handling

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/LobbyPlayer.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/LobbyPlayer.java
@@ -3,12 +3,20 @@ package dk.dtu.compute.se.pisd.roborally.model;
 public class LobbyPlayer {
     public Long id;
     public String name;
-    public PlayerState state;
+    private PlayerState state = PlayerState.NOT_READY;
+    private boolean isReady = false;
     public Long gameId;
 
-    public LobbyPlayer(){
-        this.state = PlayerState.NOT_READY;
-        this.gameId = 0L;
+    public LobbyPlayer(long gameId){this.gameId = gameId;}
+    public LobbyPlayer(String name, long gameId){
+        this.name = name;
+        this.gameId = gameId;
     }
+    public void setState(PlayerState state){
+        this.state = state;
+        isReady = (state == PlayerState.READY);
+    }
+    public boolean isReady(){return isReady;}
+    public PlayerState getState(){return state;}
 }
 

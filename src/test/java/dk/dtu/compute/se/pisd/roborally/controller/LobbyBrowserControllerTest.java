892cccc167ed2ae450d681ad80859b56d1cbc28b
Sofus Jejlskov Brandt <s214972@dtu.dk>
2024-06-19 13:19:29 +0200

    Create test for LobbyBrowserController.connectToServer

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/LobbyBrowserControllerTest.java
@@ -0,0 +1,30 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.RoboRally;
+import dk.dtu.compute.se.pisd.roborally.config.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class LobbyBrowserControllerTest {
+    @Test
+    public void testConnectToServer() {
+        LobbyBrowserController lobbyBrowserController = new LobbyBrowserController(new AppController(new RoboRally()));
+        String ip;
+
+        // Test positive scenario
+        ip = "localhost";
+        Assertions.assertTrue(lobbyBrowserController.connectToServer(ip));
+        Assertions.assertEquals(AppConfig.getProperty("server.base.url"), "http://" + ip + ":8080/api");
+        Assertions.assertEquals(AppConfig.getProperty("server.games.url"), "http://" + ip + ":8080/api/games");
+        Assertions.assertEquals(AppConfig.getProperty("server.moves.url"), "http://" + ip + ":8080/api/moves");
+        Assertions.assertEquals(AppConfig.getProperty("server.players.url"), "http://" + ip + ":8080/api/players");
+
+        // Test negative scenario
+        ip = "test123";
+        Assertions.assertFalse(lobbyBrowserController.connectToServer(ip));
+        Assertions.assertNotEquals(AppConfig.getProperty("server.base.url"), "http://" + ip + ":8080/api");
+        Assertions.assertNotEquals(AppConfig.getProperty("server.games.url"), "http://" + ip + ":8080/api/games");
+        Assertions.assertNotEquals(AppConfig.getProperty("server.moves.url"), "http://" + ip + ":8080/api/moves");
+        Assertions.assertNotEquals(AppConfig.getProperty("server.players.url"), "http://" + ip + ":8080/api/players");
+    }
+}

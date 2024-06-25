0ad3214503511de1d620a348cecc2dccb7df94a7
Adrian Macauley (s225733) <S225733@dtu.dk>
2024-06-25 18:02:26 +0200

    Merged tests into dev

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
@@ -264,13 +264,14 @@ public class Player extends Subject {
             checkpoints.add(checkpoint);
 
             // Remove the CheckpointView on the Board if we are the local player
-            if(checkpoint.getCheckpointView() != null && Objects.equals(this.id, localPlayer.getId())){{
-                checkpoint.getCheckpointView().setCheckpointAsReached();
-                notifyChange();
+            if (checkpoint.getCheckpointView() != null && Objects.equals(this.id, localPlayer.getId())) {
+                {
+                    checkpoint.getCheckpointView().setCheckpointAsReached();
+                    notifyChange();
+                }
             }
         }
     }
-}
     /**
      * Returns all checkpoints associated with the player.
      *
